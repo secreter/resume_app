ea0523473193a9226f4852c6ea5e75c2c17460fd
So <1979510177@qq.com>
2016-12-14 00:13:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
